4d49d7e974ffba38fe6206b06621577b8763a1ce
Grant Hesketh <ghesketh76@gmail.com>
2020-11-05 21:22:43 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,15 @@
 # Write your methods here
+
+def loop_message_five_times(message)
+  count = 0 
+  
+  while count < 5 do 
+    puts message
+    counter += 1 
+  end
+end
+
+def loop_message_n_times (message,number)
+  counter = 0 
+  
+  while

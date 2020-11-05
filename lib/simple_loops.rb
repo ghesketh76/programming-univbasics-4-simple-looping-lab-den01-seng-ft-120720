434949541a481b34e81cd130eb5facacc30f6596
Grant Hesketh <ghesketh76@gmail.com>
2020-11-05 21:29:10 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -18,3 +18,12 @@ def loop_message_n_times (message, number)
     count +=1 
   end
 end
+
+def output_array (array)
+  count = 0 
+  
+  while array[count] do
+    puts array[count]
+    count += 1
+  end
+end
